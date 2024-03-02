e0842777896841065e20ffa9ed6ce78d148ac18e
Jcolltech <jkoplik@albany.edu>
2024-03-02 11:07:00 -0500

    Convert RSVP schema to reference objects, and implement rsvp update method

--- backend/models/Rsvp.js
+++ backend/models/Rsvp.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const rsvpSchema = new mongoose.Schema({
-    event: { type: String, required: true},
-    user: { type: String, required: true},
+    event: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     status: { type: String, required: true},
     note: {type: String},
-    // Note: anything that may be important for FUTURE LOGIC (i.e. is a clear identnfier, most things other tha "message")
+    // Note: anything that may be important for FUTURE LOGIC (i.e. is a clear identnfier, most things other the "message")
     guestsBringing: { type: Number, required: true}
 });
 
